a7dcb781def4f930b679cb576a4b5d9c226363c9
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-18 15:47:50 +0600

    7 Set logged in user in state, display logged in user info

--- src/App.js
+++ src/App.js
@@ -3,22 +3,48 @@ import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
   const provider = new firebase.auth.GoogleAuthProvider();
-  const handleSignIn = () =>{
+  const handleSignIn = () => {
     firebase.auth().signInWithPopup(provider)
-    .then(res => {
-      // console.log(res);
-      const {displayName, photoURL, email} = res.user;
-      console.log(displayName,email,photoURL);
-    })
+      .then(res => {
+        // console.log(res);
+        const { displayName, photoURL, email } = res.user;
+        // console.log(displayName,email,photoURL);
+        const signInUser = {
+          isSignIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signInUser)
+        // console.log(displayName,email,photoURL);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
   }
   return (
     <div className="App">
       <button onClick={handleSignIn}>Sign in</button>
+      {
+        user.isSignIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt=""/>
+        </div>
+      }
     </div>
   );
 }
